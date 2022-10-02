c6198bafdaaf9cf914bf2e7739790f294b77053d
Nguyen Nam Anh <114069862+RynxHn@users.noreply.github.com>
2022-10-02 15:44:32 +0700

    Add files via upload

---
+++ Buoi5/BT_Tai_Lop/CodeFile1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+namespace BT_Tai_Lop
+{
+    class B1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Nhap_n(out n);
+            ArrayList lst = new ArrayList();
+            input(lst, n);
+            Console.WriteLine("TBC cua mang: " + TBC(lst));
+            //Timx(lst);
+            Themx(lst, n);
+        }
+        static void Nhap_n(out int n)
+        {
+            Console.Write("Nhap n: ");
+            n = int.Parse(Console.ReadLine());
+        }
+        static void input(ArrayList lst, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    Console.Write("Nhap ptu thu {0}:", i);
+                    lst.Add(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Nhap sai! Nhap lai ptu thu {0}:", i);
+                    lst.Add(int.Parse(Console.ReadLine()));
+                }
+            }
+        }
+        static void output(ArrayList lst)
+        {
+            for (int i = 0; i < lst.Count; i++)
+                Console.Write(lst[i] + "\t");
+        }
+        static float TBC(ArrayList lst)
+        {
+            int s = 0,d =0;
+            for(int i=0; i<lst.Count; i++)
+            {
+                s += int.Parse(lst[i].ToString());
+                d++;
+            }
+            return (float)s / d;
+        }
+        static void Timx(ArrayList lst)
+        {
+            int x;
+            Console.Write("Nhap x: ");
+            x = int.Parse(Console.ReadLine());
+            for(int i=0; i < lst.Count; i++)
+                if(int.Parse(lst[i].ToString()) == x)
+                    Console.WriteLine("{0} co trong mang, o vi tri {1}", x, lst[i]);
+        }
+        /*
+        static void Themx(ArrayList lst, int n)
+        {
+            int m = 0, x = 0;
+            Console.Write("Nhap x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap m: ");
+            m = int.Parse(Console.ReadLine());
+            int[] newarr = new int[n + 1];
+            for(int i=0; i <n; i++)
+            {
+                
+            }
+            lst[m] = x;
+            n++;
+            output(lst);
+        }*/
+    }
+}
---
+++ Buoi5/BT_Tai_Lop/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+/*
+namespace BT_Tai_Lop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Nhap_n(out n);
+            ArrayList lst = new ArrayList();
+            Nhap(lst, n);
+            Console.Write("1.Mang vua nhap: ");
+            Xuat(lst);
+            Console.WriteLine("\n2.Mang co {0} so chan", demchan(lst));
+            if (boi5(lst))
+                Console.WriteLine("3.Mang co ton tai so chia het 5");
+            else
+                Console.WriteLine("3.Mang ko ton tai so chia het 5");
+        }
+        static void Nhap_n(out int n)
+        {
+            Console.Write("Nhap n: ");
+            n = int.Parse(Console.ReadLine());
+        }
+        static void Nhap(ArrayList lst, int n)
+        {
+            for(int i=0; i < n; i++)
+            {
+                Console.Write("Nhap ptu thu {0}:", i);
+                lst.Add(Console.ReadLine());
+            }
+        }
+        static void Xuat(ArrayList lst)
+        {
+            for (int i = 0; i < lst.Count; i++)
+                Console.Write(lst[i] + "\t");
+        }
+        static int demchan(ArrayList lst)
+        {
+            int d = 0;
+            for (int i = 0; i < lst.Count; i++)
+                if (int.Parse(lst[i].ToString()) % 2 == 0)
+                    d++;
+            return d;
+        }
+        static int tongam(ArrayList lst)
+        {
+            int s = 0;
+            for (int i = 0; i < lst.Count; i++)
+                if (int.Parse(lst[i].ToString()) < 0)
+                    s = s + int.Parse(lst[i].ToString());
+            return s;
+        }
+        static bool boi5(ArrayList lst)
+        {
+            for (int i = 0; i < lst.Count; i++)
+                if (int.Parse(lst[i].ToString()) % 5 == 0)
+                    return true;
+            return false;
+        }
+    }
+}
+*/
